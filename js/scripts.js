c16ff31424cfbc653e0ce279a5d2f01cdbba3d46
annapgsm <anna.pingsmann@gmail.com>
2025-07-16 16:38:31 +0200

    updated scripts.js
    added pokemonList array with several objects

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,41 @@
-alert('Hello World');
+// Create a new variable (pokemonList) and assign a blank array to it
+let pokemonList = [];
 
-let favoriteFood='Pineapple';
-document.write(favoriteFood);
+// Add several Pokémon objects to the array
+pokemonList = [
+  {
+    name: 'Bulbasaur',
+    height: 7,
+    types: ['grass', 'poison'],
+    speed: 45,
+    catchRate: 45
+  },
+  {
+    name: 'Charmander',
+    height: 6,
+    types: ['fire'],
+    speed: 65,
+    catchRate: 45
+  },
+  {
+    name: 'Squirtle',
+    height: 5,
+    types: ['water'],
+    speed: 43,
+    catchRate: 45
+  },
+  {
+    name: 'Turtwig',
+    height: 4,
+    types: ['grass'],
+    speed: 31,
+    catchRate: 45
+  },
+  {
+    name: 'Pikachu',
+    height: 4,
+    types: ['electric'],
+    speed: 90,
+    catchRate: 190
+  }
+];
