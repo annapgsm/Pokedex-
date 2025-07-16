f07b05cebfda587c8d4304010fe3d7d3027458a8
annapgsm <anna.pingsmann@gmail.com>
2025-07-16 17:54:57 +0200

    updated scripts.js
    Added loop
    changed heights from 7 to 0.7

--- js/scripts.js
+++ js/scripts.js
@@ -5,37 +5,57 @@ let pokemonList = [];
 pokemonList = [
   {
     name: 'Bulbasaur',
-    height: 7,
+    height: 0.7,
     types: ['grass', 'poison'],
     speed: 45,
     catchRate: 45
   },
   {
     name: 'Charmander',
-    height: 6,
+    height: 0.6,
     types: ['fire'],
     speed: 65,
     catchRate: 45
   },
   {
     name: 'Squirtle',
-    height: 5,
+    height: 0.5,
     types: ['water'],
     speed: 43,
     catchRate: 45
   },
   {
     name: 'Turtwig',
-    height: 4,
+    height: 0.4,
     types: ['grass'],
     speed: 31,
     catchRate: 45
   },
   {
     name: 'Pikachu',
-    height: 4,
+    height: 0.4,
     types: ['electric'],
     speed: 90,
     catchRate: 190
   }
 ];
+
+// Start a loop that goes through each item in the pokemonList array
+for (let i = 0; i < pokemonList.length; i++) {
+
+  // Stores the current Pokémon object in a variable
+  let pokemon = pokemonList[i];
+
+  // Creates a string that combines the name and height
+  let displayText = pokemon.name + " (height: " + pokemon.height + ")";
+
+  // Checks if the Pokémon is tall (height greater than 0.6)
+  if (pokemon.height > 0.6) {
+    // If so, adds a special message to the string
+    displayText += " - Wow, that's big!";
+  }
+
+  // Write the final string to the web page and add a line break
+  document.write(displayText + "<br>");
+}
+
